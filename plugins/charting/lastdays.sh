bf35c9d554e716da20d8ffc9f51512fd68124868
Buote Xu <Buote.Xu@gmail.com>
2014-01-15 19:50:59 +0100

    blub

--- plugins/charting/lastdays.sh
+++ plugins/charting/lastdays.sh
@@ -4,6 +4,7 @@ a=""
 b=""
 for i in $(seq 0 30)
 do
+    echo 'git diff --shortstat "master@{ $i day ago }..master@{now}"'
     b=$(git diff --shortstat "master@{ $i day ago }..master@{now}")
     if [[ "$b" != "$a" ]]; then
         echo $(date --date="$i days ago" +%F) $b
